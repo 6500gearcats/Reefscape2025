f21bb28e657c72339b0e04328ee3b376e0fb2cdd
Seth Bragdon <sethbragdon1@gmail.com>
2025-01-12 16:23:13 -0500

    Added command to set elevator speed.

---
+++ src/main/java/frc/robot/Subsystems/Elevator.java
@@ -0,0 +1,64 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import com.revrobotics.spark.SparkMax;
+
+import au.grapplerobotics.ConfigurationFailedException;
+import au.grapplerobotics.LaserCan;
+
+import com.revrobotics.spark.SparkLowLevel;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase {
+  // TODO add correct ids
+  private SparkMax m_elevatorMotor = new SparkMax(0, SparkLowLevel.MotorType.kBrushless);
+  private LaserCan m_elevatorLidar = new LaserCan(0);
+  private DigitalInput m_elevatorLimitSwitch = new DigitalInput(0);
+
+  
+  /** Creates a new Elevator. */
+  public Elevator() {
+    try {
+      m_elevatorLidar.setRangingMode(LaserCan.RangingMode.SHORT);
+      m_elevatorLidar.setRegionOfInterest(new LaserCan.RegionOfInterest(8, 8, 16, 16));
+      m_elevatorLidar.setTimingBudget(LaserCan.TimingBudget.TIMING_BUDGET_33MS);
+    } 
+    catch(ConfigurationFailedException e){
+      System.out.println("So... Uh... The laser didn't work. Seth error." + e);
+    }
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Elevator Height (m)", this.getElevatorHeight());
+    SmartDashboard.putBoolean("Elevator Limit Reached", elevatorAtLimit());
+  }
+
+  // Return the height of the elevator in meters
+  public double getElevatorHeight(){
+    LaserCan.Measurement measurement = m_elevatorLidar.getMeasurement();
+    if(measurement.status == LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT)
+    {
+      return (double)measurement.distance_mm/1000.0;
+    }
+    return 0;
+  }
+
+  // Set the elevator speed
+  public void setElevatorSpeed(double speed){
+    m_elevatorMotor.set(speed);
+  }
+
+  // Check to see if the elevator is too high
+  public boolean elevatorAtLimit()
+  {
+    return m_elevatorLimitSwitch.get();
+  }
+}
