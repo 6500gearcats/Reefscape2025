3ee5b1d32126992ee7d3ceb5886e6dcce58751cf
Asher Leffler <asher.leffler@gmail.com>
2025-01-31 15:24:48 -0500

    Updated vendordeps and adjust package names to be lowercase

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.Subsystems.DriveSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.Vision;
 
 public class RobotContainer {
