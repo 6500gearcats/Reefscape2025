8f1cebefa765f7aa38aa3ea5746584804e9f1a37
Seth Bragdon <sethbragdon1@gmail.com>
2025-01-31 15:34:12 -0500

    Changed the folder name to prevent error.

--- src/main/java/frc/robot/Commands/SetElevatorSpeed.java
+++ src/main/java/frc/robot/Commands/SetElevatorSpeed.java
@@ -6,7 +6,7 @@ package frc.robot.Commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import frc.robot.Subsystems.Elevator;
+import frc.robot.subsystems.Elevator;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class SetElevatorSpeed extends Command {
