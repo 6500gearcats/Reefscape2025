5b178f8c815458dd0b03a1b8ea32999097fc4789
Seth Bragdon <sethbragdon1@gmail.com>
2025-02-07 18:28:35 -0500

    Removed subsystems that are non-applicable to the new robot design

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,19 +13,19 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.MoveClimberArm;
 import frc.robot.commands.Algae.IntakeAlgae;
-import frc.robot.commands.Aligner.AlignCoral;
+//import frc.robot.commands.Aligner.AlignCoral;
 import frc.robot.commands.Arm.SetArmPosition;
-import frc.robot.commands.CommandGroups.AlignAndFeed;
-import frc.robot.commands.CommandGroups.IntakeFromGround;
-import frc.robot.commands.GroundIntake.FlipGroundIntakeIn;
-import frc.robot.commands.GroundIntake.FlipGroundIntakeOut;
+//import frc.robot.commands.CommandGroups.AlignAndFeed;
+//import frc.robot.commands.CommandGroups.IntakeFromGround;
+//import frc.robot.commands.GroundIntake.FlipGroundIntakeIn;
+//import frc.robot.commands.GroundIntake.FlipGroundIntakeOut;
 import frc.robot.commands.SourceIntake.ShootCoral;
 import frc.robot.subsystems.AlgaeIntake;
-import frc.robot.subsystems.Aligner;
+//import frc.robot.subsystems.Aligner;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.CoralHolder;
-import frc.robot.subsystems.GroundIntake;
+//import frc.robot.subsystems.GroundIntake;
 
 public class RobotContainer {
   private final Climber m_climber = new Climber();
@@ -34,8 +34,8 @@ public class RobotContainer {
 
   private final AlgaeIntake m_algaeIntake = new AlgaeIntake();
 
-  private final GroundIntake m_groundIntake = new GroundIntake();
-  private final Aligner m_aligner = new Aligner();
+  //private final GroundIntake m_groundIntake = new GroundIntake();
+  //private final Aligner m_aligner = new Aligner();
   private final CoralHolder m_coralHolder = new CoralHolder();
   private final Arm m_arm = new Arm();
   
@@ -44,15 +44,15 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    new JoystickButton(m_gunner, Button.kA.value).whileTrue(new IntakeFromGround(m_groundIntake))
-      .onFalse(new AlignAndFeed(m_groundIntake, m_aligner, m_coralHolder));
+  //  new JoystickButton(m_gunner, Button.kA.value).whileTrue(new IntakeFromGround(m_groundIntake))
+  //    .onFalse(new AlignAndFeed(m_groundIntake, m_aligner, m_coralHolder));
     new JoystickButton(m_gunner, Button.kB.value).whileTrue(new IntakeAlgae(m_algaeIntake, .05));
-    new JoystickButton(m_gunner, Button.kY.value).whileTrue(new AlignCoral(m_aligner, .5));
+  //  new JoystickButton(m_gunner, Button.kY.value).whileTrue(new AlignCoral(m_aligner, .5));
     new JoystickButton(m_gunner, Button.kX.value).whileTrue(new SetArmPosition(m_arm, 30));
     new JoystickButton(m_driver, Button.kA.value).whileTrue(new ShootCoral(m_coralHolder, 0.8)); // Simulation Reasons
     new JoystickButton(m_driver, Button.kB.value).whileTrue(new MoveClimberArm(m_climber,0.9)); // Simulation Reasons
-    new JoystickButton(m_driver, Button.kX.value).whileTrue(new FlipGroundIntakeIn(m_groundIntake,0.2)); // Simulation Reasons
-    new JoystickButton(m_driver, Button.kY.value).whileTrue(new FlipGroundIntakeOut(m_groundIntake, -0.2));
+    //new JoystickButton(m_driver, Button.kX.value).whileTrue(new FlipGroundIntakeIn(m_groundIntake,0.2)); // Simulation Reasons
+    //new JoystickButton(m_driver, Button.kY.value).whileTrue(new FlipGroundIntakeOut(m_groundIntake, -0.2));
   }
 
   public Command getAutonomousCommand() {
