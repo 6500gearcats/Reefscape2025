a1212af0c96ed1b524cb319d6da5feae27d81176
Seth Bragdon <sethbragdon1@gmail.com>
2025-02-12 12:03:46 -0500

    Merged climber to main.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -47,9 +47,9 @@ public final class Constants {
 
     // Chassis configuration
     // Distance between centers of right and left wheels on robot
-    public static final double kTrackWidth = Units.inchesToMeters(23.5);
+    public static final double kTrackWidth = Units.inchesToMeters(25);
     // Distance between front and back wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(28.5);
+    public static final double kWheelBase = Units.inchesToMeters(25);
 
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
         new Translation2d(kWheelBase / 2, kTrackWidth / 2),
