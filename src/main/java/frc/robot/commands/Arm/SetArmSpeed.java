1a600eb9ec8ec3fffd04c654bcdda3f32e343b26
Seth Bragdon <sethbragdon1@gmail.com>
2025-02-12 11:05:41 -0500

    Created testing branch for arm.

--- src/main/java/frc/robot/commands/Arm/SetArmSpeed.java
+++ src/main/java/frc/robot/commands/Arm/SetArmSpeed.java
@@ -2,42 +2,44 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Algae;
+package frc.robot.commands.Arm;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.AlgaeIntake;
+import frc.robot.subsystems.Arm;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class IntakeAlgae extends Command {
-  AlgaeIntake m_algaeIntake;
-  double speed;
-  /** Creates a new IntakeAlgae. */
-  public IntakeAlgae(AlgaeIntake m_algaeIntake, double speed) {
+public class SetArmSpeed extends Command {
+  /** Creates a new SetArmPOsition. */
+  Arm m_arm;
+  double m_speed;
+  public SetArmSpeed(Arm arm, double speed) {
     // Use addRequirements() here to declare subsystem dependencies.
-    this.m_algaeIntake = m_algaeIntake;
-    this.speed = speed;
-    addRequirements(m_algaeIntake);
+    m_arm = arm;
+    m_speed = speed;
+    addRequirements(m_arm);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_algaeIntake.intakeAlgae(speed);
+    m_arm.spinArm(m_speed);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_algaeIntake.intakeAlgae(0);
+    m_arm.spinArm(0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_algaeIntake.hasAlgae();
+    return false;
   }
 }
