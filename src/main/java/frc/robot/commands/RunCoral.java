bf2cdbdd625078aea36fb90870a105944c72236e
Jason Holt <you@example.com>
2025-02-23 13:23:02 -0500

    Seemingly got the pathfind logic working! (must hold button)

--- src/main/java/frc/robot/commands/RunCoral.java
+++ src/main/java/frc/robot/commands/RunCoral.java
@@ -2,14 +2,19 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
 package frc.robot.commands;
+
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Optional;
 
+
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathfindingCommand;
 import com.pathplanner.lib.path.PathConstraints;
+
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
@@ -19,77 +24,113 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.subsystems.DriveSubsystem;
 
+
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+
+
 public class RunCoral extends ParallelDeadlineGroup {
-  /** Creates a new RunCoralRight. */
-  AprilTagFieldLayout field;
-  DriveSubsystem m_drive;
+
+
+    AprilTagFieldLayout field;
+    DriveSubsystem m_drive;
   public RunCoral(DriveSubsystem m_drive) {
-    // Add the deadline command in the super() call. Add other commands usin
-    super(new IsRobotAtPosition(m_drive));
-    addCommands(AutoBuilder.pathfindToPose(getBestAprilTag(m_drive), new PathConstraints(3.0, 3.0, Units.degreesToRadians(540), Units.degreesToRadians(720))));
+      super(new IsRobotAtPosition(m_drive));
+      this.m_drive = m_drive;
+      System.out.println("RunCoral Command Initialized");
+      addCommands(new PathfindingCommand(m_drive));
   }
 
-  private Pose2d getBestAprilTag(DriveSubsystem m_drive) {
-    field = AprilTagFields.kDefaultField.loadAprilTagLayoutField();
-    Pose2d pose = m_drive.getPose();
-    int bestAprilTag = getClosestAprilTagID(pose.getTranslation());
-    Pose2d newPose = field.getTagPose(bestAprilTag).get().toPose2d();
-    System.out.println("Old Poses values" + newPose.getX() + ", " + newPose.getY() + ". Rotation: " + newPose.getRotation());
 
-    double tempAngle = field.getTagPose(bestAprilTag).get().toPose2d().getRotation().getRadians();
-    double newX = 0;
-    double newY = 0;
-    newX = (newPose.getX() + Math.cos(tempAngle) * .66) + Math.cos(tempAngle + Math.PI/2) * .3;
-    newY = (newPose.getY() + Math.sin(tempAngle) * .66) + Math.sin(tempAngle + Math.PI/2) * .3;
-    Pose2d thirdPose = new Pose2d(newX, newY, newPose.getRotation().plus(new Rotation2d(Math.PI)));
-    System.out.println("New Poses values" + thirdPose.getX() + ", " + thirdPose.getY() + ". Rotation: " + thirdPose.getRotation());
-    return thirdPose;
+public class PathfindingCommand extends Command {
+  public PathfindingCommand(DriveSubsystem m_drive) {
+      addRequirements(m_drive);
+      System.out.println("PathfindingCommand Created");
   }
 
-  private int getClosestAprilTagID(Translation2d robotPose) {
-    int integer = 0;
-    ArrayList<Double> poses = new ArrayList<Double>();
-    ArrayList<Integer> list = new ArrayList<Integer>();
-    Optional<Alliance> alliance = DriverStation.getAlliance();
-    if (alliance.isPresent()) {
-        if (alliance.get() == Alliance.Red) {
-          for (int i = 6; i<12; i++) {
-            Translation2d pose = field.getTagPose(i).get().getTranslation().toTranslation2d();
-            double distance = robotPose.getDistance(pose);
-            poses.add(distance);
-            list.add(i);
-          }
-        }
-        if (alliance.get() == Alliance.Blue) {
-          for (int i = 17; i<23; i++) {
-            Translation2d pose = field.getTagPose(i).get().getTranslation().toTranslation2d();
-            double distance = robotPose.getDistance(pose);
-            poses.add(distance);
-            list.add(i);
-          }
-        }
+
+  @Override
+  public void initialize() {
+      System.out.println("Pathfinder Initialized");
+  }
+
+
+  @Override
+  public void execute() {
+      System.out.println("Doing the Pathfinding");
+      Pose2d robotPose = m_drive.getPose();
+      Pose2d targetPose = getBestAprilTag(robotPose);
+      System.out.println("Target Pose: " + targetPose);
+      AutoBuilder.pathfindToPose(targetPose, new PathConstraints(3.0, 3.0, Units.degreesToRadians(540), Units.degreesToRadians(720))).schedule();;
     }
-    else {
-      for (int i = 1; i<23; i++) {
-        Translation2d pose = field.getTagPose(i).get().getTranslation().toTranslation2d();
-        double distance = robotPose.getDistance(pose);
-        poses.add(distance);
-        list.add(i);
-      }
-    }
-    double minValue = Collections.min(poses);
-    System.out.println("Min value: " + minValue);
-    integer = poses.indexOf(minValue);
-    System.out.println("AprilTag of least distance: " + list.get(integer));
-    return list.get(integer);
+ 
+
+
+  @Override
+  public void end(boolean interrupted) {
+      System.out.println("Pathfinding Done");
   }
 
-  
+
+  @Override
+  public boolean isFinished() {
+      return false;
+  }
+    }
+    private Pose2d getBestAprilTag(Pose2d robotPose) {
+        field = AprilTagFields.kDefaultField.loadAprilTagLayoutField();
+        int bestAprilTag = getClosestAprilTagID(robotPose.getTranslation());
+        Pose2d tagPose = field.getTagPose(bestAprilTag).get().toPose2d();
+
+
+        double tempAngle = tagPose.getRotation().getRadians();
+        double newX = tagPose.getX() + Math.cos(tempAngle) * 0.66 + Math.cos(tempAngle + Math.PI / 2) * 0.3;
+        double newY = tagPose.getY() + Math.sin(tempAngle) * 0.66 + Math.sin(tempAngle + Math.PI / 2) * 0.3;
+
+
+        return new Pose2d(newX, newY, tagPose.getRotation());
+    }
+
+
+    private int getClosestAprilTagID(Translation2d robotPose) {
+        int integer = 0;
+        ArrayList<Double> distances = new ArrayList<>();
+        ArrayList<Integer> tags = new ArrayList<>();
+        Optional<Alliance> alliance = DriverStation.getAlliance();
+
+
+        if (alliance.isPresent()) {
+            int startTag = (alliance.get() == Alliance.Red) ? 6 : 17;
+            int endTag = (alliance.get() == Alliance.Red) ? 12 : 23;
+
+
+            for (int i = startTag; i < endTag; i++) {
+                Translation2d tagPose = field.getTagPose(i).get().getTranslation().toTranslation2d();
+                double distance = robotPose.getDistance(tagPose);
+                distances.add(distance);
+                tags.add(i);
+            }
+        } else {
+            for (int i = 1; i < 23; i++) {
+                Translation2d tagPose = field.getTagPose(i).get().getTranslation().toTranslation2d();
+                double distance = robotPose.getDistance(tagPose);
+                distances.add(distance);
+                tags.add(i);
+            }
+        }
+
+
+        double minDistance = Collections.min(distances);
+        integer = distances.indexOf(minDistance);
+        return tags.get(integer);
+    }
 }
+
+
+
