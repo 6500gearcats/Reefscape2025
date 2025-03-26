0050a14c2bd8d0539a30cdae25033b58b62e256f
Rob Gale <rob.gale@fujitsu.com>
2025-03-26 16:51:14 -0400

    Use 2 try blocks

--- src/main/java/frc/robot/commands/ProportionalAlign.java
+++ src/main/java/frc/robot/commands/ProportionalAlign.java
@@ -98,12 +98,17 @@ public class ProportionalAlign extends Command {
       dx = targetX - m_drive.getPose().getX();
       dy = targetY - m_drive.getPose().getY();
 
+      double currentAngle = m_drive.getAngle();
       // Logics to mofidy the targetAngle- localizes the angle to between -180 and 180
       // and take most efficient path in a very complicated way
-      dr = targetAngle - (Math.abs((m_drive.getAngle() % 360)) * (m_drive.getAngle() / Math.abs(m_drive.getAngle()))
-          - 180 * (m_drive.getAngle() / Math.abs(m_drive.getAngle())));
+      dr = targetAngle - (Math.abs((currentAngle % 360)) * (currentAngle / Math.abs(currentAngle))
+          - 180 * (currentAngle/ Math.abs(currentAngle)));
       // dr = (Math.abs(dr) -180) * (Math.abs(dr)/dr);
+    } catch (Exception e) {
+      System.out.println("Exception in ProportionalAlign A" + e.getMessage());
+    }
 
+    try {
       // Takes the total sum of errors of x and y direction to use for slowing down
       // the robot
       double total = Math.abs(dx) + Math.abs(dy);
@@ -156,7 +161,7 @@ public class ProportionalAlign extends Command {
             dr / drModifier, true, "Proportional Alignment 3");
       }
     } catch (Exception e) {
-      System.console().writer().println("Exception in ProportionalAlign" + e.getMessage());
+      System.out.println("Exception in ProportionalAlign B" + e.getMessage());
     }
   }
 
