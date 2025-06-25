e3dbd2b414f8f96a5487af7a9dd03e2193646ed1
gearcat6500 <gearcats6500programming@gmail.com>
2025-06-25 16:52:07 -0400

    Code for swerve drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,84 +4,72 @@
 
 package frc.robot;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Optional;
-import java.util.function.IntSupplier;
-
-import org.photonvision.PhotonCamera;
+import java.io.File;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.path.PathConstraints;
-
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.estimator.PoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PS4Controller.Button;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.POVButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.IntakeCommand;
-import frc.robot.commands.IntakeNote;
-import frc.robot.commands.Shoot;
-import frc.robot.commands.ShootNote;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveInputStream;
 import frc.robot.subsystems.Shooter;
 public class RobotContainer {
 
   AprilTagFieldLayout field;
   Pose2d newPose = new Pose2d();
 
-  private final SendableChooser<Command> autoChooser;
+  //private final SendableChooser<Command> autoChooser;
 
   XboxController m_driver = new XboxController(0);
   XboxController m_gunner = new XboxController(1);
 
-  
+  private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/neo"));
   //Temporarily adding this to
+    /**
+   * Converts driver input into a field-relative ChassisSpeeds that is controlled by angular velocity.
+   */
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
+                                                                () -> m_driver.getLeftY() * -1,
+                                                                () -> m_driver.getLeftX() * -1)
+                                                            .withControllerRotationAxis(m_driver::getRightX)
+                                                            .deadband(0.1)
+                                                            .scaleTranslation(0.8)
+                                                            .allianceRelativeControl(true);
  
   Intake m_intake = new Intake();
   Shooter m_shooter = new Shooter();
 
 
   public RobotContainer() {
-    //* zero heading
    
 
     // Build an auto chooser. This will use Commands.none() as the default option.
-    autoChooser = AutoBuilder.buildAutoChooser();
+    //autoChooser = AutoBuilder.buildAutoChooser();
 
     configureBindings();
     // *default command
     
+    
   }
 
   private void configureBindings() {
+
+    Command driveFieldOrientedAnglularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
+
+    drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
     // Configure your button bindings here
-    new JoystickButton(m_gunner, XboxController.Button.kA.value).whileTrue(new IntakeNote(m_intake).andThen(new IntakeCommand(m_intake, -0.2).withTimeout(0.2)));
-    new JoystickButton(m_gunner, XboxController.Button.kX.value).whileTrue(new Shoot(m_shooter));
-    new JoystickButton(m_gunner, XboxController.Button.kY.value).whileTrue(new Shoot(m_shooter).withTimeout(1.2).andThen((new ShootNote(m_intake, m_shooter))));
+    //new JoystickButton(m_gunner, XboxController.Button.kA.value).whileTrue(new IntakeNote(m_intake).andThen(new IntakeCommand(m_intake, -0.2).withTimeout(0.2)));
+    //new JoystickButton(m_gunner, XboxController.Button.kX.value).whileTrue(new Shoot(m_shooter));
+    //new JoystickButton(m_gunner, XboxController.Button.kY.value).whileTrue(new Shoot(m_shooter).withTimeout(1.2).andThen((new ShootNote(m_intake, m_shooter))));
   }
 
   public Command getAutonomousCommand() {
-    return autoChooser.getSelected();
+    return null;
+    //return autoChooser.getSelected();
   }
 
   public void resetRobotGyroAndOrientation() {
