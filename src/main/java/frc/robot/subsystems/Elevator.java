71037e042992407d39c1ded86a5b5a7297a78310
Jason Holt <you@example.com>
2025-10-11 11:54:55 -0400

    Elevator magic motion maybe

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -22,6 +22,8 @@ import frc.robot.Constants;
 import frc.robot.Robot;
 
 import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicExpoVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Elevator extends SubsystemBase {
@@ -51,6 +53,31 @@ public class Elevator extends SubsystemBase {
     catch(ConfigurationFailedException e){
       System.out.println("So... Uh... The laser didn't work. Seth error." + e);
     }
+    // in init function
+var talonFXConfigs = new TalonFXConfiguration();
+
+// set slot 0 gains
+var slot0Configs = talonFXConfigs.Slot0;
+slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
+slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+slot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
+slot0Configs.kI = 0; // no output for integrated error
+slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+
+// set Motion Magic Expo settings
+var motionMagicConfigs = talonFXConfigs.MotionMagic;
+motionMagicConfigs.MotionMagicCruiseVelocity = 0; // Unlimited cruise velocity
+motionMagicConfigs.MotionMagicExpo_kV = 0.12; // kV is around 0.12 V/rps
+motionMagicConfigs.MotionMagicExpo_kA = 0.1; // Use a slower kA of 0.1 V/(rps/s)
+
+m_elevatorMotor.getConfigurator().apply(talonFXConfigs);
+
+// create a Motion Magic Expo request, voltage output
+final MotionMagicExpoVoltage m_request = new MotionMagicExpoVoltage(0);
+
+// set target position to 100 rotations
+m_elevatorMotor.setControl(m_request.withPosition(-24));
   }
 
   @Override
